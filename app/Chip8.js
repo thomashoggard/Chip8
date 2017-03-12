bfc28c4a3fb1c33812e212891be932327ada2bd5
Thomas Hoggard <Thomas.Hoggard@gmail.com>
2017-03-11 20:54:12 -0500

    completed more opcodes'

--- app/Chip8.js
+++ app/Chip8.js
@@ -11,9 +11,11 @@ class Chip8 {
     }
 
     reset() {
-        this.memory = new Array(4096);
-        this.stack = new Array(16);
-        this.sp = 0;
+        this.memory = new Uint8Array(4096);
+        this.pc = 0x200; // Program Counter
+        
+        this.stack = new Uint16Array(16);
+        this.sp = 0; // Stack Pointer
         
         // Registers
         this.V = new Uint8Array(16);
@@ -24,7 +26,8 @@ class Chip8 {
         // Graphics
         this.displayWidth = 64;
         this.displayHeight = 32;
-        this.display = new Array(this.displayWidth * this.displayHeight);
+        // Only uses the least significant bit
+        this.display = new Uint8Array(this.displayWidth * this.displayHeight);
     }
 
     executeOpcode(opcode) {
@@ -35,7 +38,35 @@ class Chip8 {
         // 8-bit constant
         const kk = opcode & 0x00FF;
 
+        // memory address
+        const nnn = opcode & 0x0FFF;
+
         switch (opcode & 0xF000) {
+            case 0x0000:
+                switch (opcode & 0x00FF) {
+                    case 0x00E0:
+                        this.clearDisplay();
+                    break;
+                    case 0x00EE:
+                        this.return();
+                    break;
+                }
+            break;
+            case 0x1000:
+                this.jumpAddress(nnn);
+            break;
+            case 0x2000:
+                this.callAddress(nnn);
+            break;
+            case 0x3000:
+                this.skipNextInstruction_Vx_Equals_kk(x, kk);
+            break;
+            case 0x4000:
+                this.skipNextInstruction_Vx_NotEquals_kk(x, kk);
+            break;
+            case 0x5000:
+                this.skipNextInstruction_Vx_Equals_Vy(x, kk);
+            break;                        
             case 0x6000: 
                 this.Set_LD_Vx_byte(x, kk);
             break;
@@ -71,9 +102,80 @@ class Chip8 {
                     break;                                                                                                    
                     case 0x000E:
                         this.Set_SHL_Vx(x);
-                    break;                         
+                    break;                        
                 }
             break;
+            case 0x9000:
+                this.skipNextInstruction_Vx_NotEquals_Vy(x, y);
+            break;
+            case 0xA000:
+                this.loadRegisterI(nnn);
+            break;
+            case 0xB000:
+                this.jumpAddressV0(nnn);
+            break;
+            case 0xC000:
+                this.randomByte_Vx(x, kk);
+            break;
+        }
+    }
+
+    // 00E0 - CLS
+    // Clear the display.
+    clearDisplay() {
+        for (var i =0; i < this.display.length; i++) {
+            this.display[i] = 0;
+        }
+    }
+
+    // 00EE - RET
+    // Return from a subroutine.
+    // The interpreter sets the program counter to the address at the top of the stack, then subtracts 1 from the stack pointer.    
+    return() {
+        this.sp--;       
+        this.pc = this.stack[this.sp];
+    }
+
+    // 1nnn - JP addr
+    // Jump to location nnn.
+    // The interpreter sets the program counter to nnn.
+    jumpAddress(nnn) {
+        this.pc = nnn;
+    }
+
+    // 2nnn - CALL addr
+    // Call subroutine at nnn.
+    // The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.
+    callAddress(nnn) {
+        this.stack[this.sp] = this.pc;
+        this.sp++;
+        this.pc = nnn;
+    }
+
+    // 3xkk - SE Vx, byte
+    // Skip next instruction if Vx = kk.
+    // The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.
+    skipNextInstruction_Vx_Equals_kk(x, kk) {
+        if (this.V[x] == kk) {
+            this.pc += 2;
+        }
+    }
+
+    // 4xkk - SNE Vx, byte
+    // Skip next instruction if Vx != kk.
+    // The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.
+    skipNextInstruction_Vx_NotEquals_kk(x, kk) {
+        if (this.V[x] != kk) {
+            this.pc += 2;
+        }
+    }
+
+    // 5xy0 - SE Vx, Vy
+    // Skip next instruction if Vx = Vy.
+    // The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.
+    skipNextInstruction_Vx_Equals_Vy(x, y) {
+        if (this.V[x] == this.V[y]) {
+            this.pc += 2;
         }
     }
 
@@ -161,8 +263,55 @@ class Chip8 {
         this.V[x] <<= 1;
     }
 
+    // 9xy0 - SNE Vx, Vy
+    // Skip next instruction if Vx != Vy.
+    // The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.    
+    skipNextInstruction_Vx_NotEquals_Vy(x, y) {
+        if (this.V[x] != this.V[y]) {
+            this.pc += 2;
+        }
+    }    
+
+    // Annn - LD I, addr
+    // Set I = nnn.
+    // The value of register I is set to nnn.    
+    loadRegisterI(nnn) {
+        this.I = nnn;
+    }
+
+    // Bnnn - JP V0, addr
+    // Jump to location nnn + V0.
+    // The program counter is set to nnn plus the value of V0.    
+    jumpAddressV0(nnn) {
+        this.pc = nnn + this.V[0];
+    }
+
+    // Cxkk - RND Vx, byte
+    // Set Vx = random byte AND kk.
+    // The interpreter generates a random number from 0 to 255, which is then ANDed with the value kk. The results are stored in Vx.
+    randomByte_Vx(x, kk) {
+        const rand = Math.floor(Math.random() * 256);
+        this.V[x] = rand & kk;
+    }
+
+    // Dxyn - DRW Vx, Vy, nibble
+    // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
+    /* The interpreter reads n bytes from memory, starting at the address stored in I. 
+        These bytes are then displayed as sprites on screen at coordinates (Vx, Vy). 
+        Sprites are XORed onto the existing screen. If this causes any pixels to be erased, 
+        VF is set to 1, otherwise it is set to 0. If the sprite is positioned so part of it is 
+        outside the coordinates of the display, it wraps around to the opposite side of the screen. 
+        See instruction 8xy3 for more information on XOR, and section 2.4, 
+        Display, for more information on the Chip-8 screen and sprites.*/
+    draw_Vx_Vy_Nibble(x, y, nibble) {
+        
+    }
+
     update(progress) {
-        // Update the state of the world for the elapsed time since last render
+        var opcode = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
+        this.pc += 2;
+
+        this.executeOpcode(opcode);
     }
 
     draw() {
--- test/test.js
+++ test/test.js
@@ -250,7 +250,199 @@ describe('Chip8', function() {
         });                             
     }); 
 
+    describe('clearDisplay', function() {
+        it (`Clear the display.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.display[0] = 1;
+
+            chip8.clearDisplay();
+
+            chip8.display[0].should.equal(0);
+            done();
+        });                                  
+    }); 
+
+    describe('jumpAddress', function() {
+        it (`The interpreter sets the program counter to nnn.`, function(done) {
+            var chip8 = new Chip8();
+
+            chip8.jumpAddress(0xFFF);
+
+            chip8.pc.should.equal(0xFFF);
+            done();
+        });                                  
+    }); 
+
+    describe('callAddress', function() {
+        it (`The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.`, function(done) {
+            var chip8 = new Chip8();
+
+            chip8.callAddress(0x300);
+
+            chip8.pc.should.equal(0x300);
+            chip8.stack[0].should.equal(0x200);
+            chip8.sp.should.equal(1);
+
+            chip8.callAddress(0x400);
+
+            chip8.pc.should.equal(0x400);
+            chip8.stack[1].should.equal(0x300);
+            chip8.sp.should.equal(2);            
+            done();
+        });                                  
+    }); 
+
+    describe('return', function() {
+        it (`The interpreter increments the stack pointer, then puts the current PC on the top of the stack. The PC is then set to nnn.`, function(done) {
+            var chip8 = new Chip8();
+
+            chip8.callAddress(0x300);
+
+            chip8.pc.should.equal(0x300);
+            chip8.stack[0].should.equal(0x200);
+            chip8.sp.should.equal(1);
+
+            chip8.return();
+          
+            chip8.pc.should.equal(0x200);
+            chip8.sp.should.equal(0);
+
+            done();
+        });                                  
+    }); 
+
+    describe('skipNextInstruction_Vx_Equals_kk', function() {
+        it (`The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+
+            chip8.skipNextInstruction_Vx_Equals_kk(0, 0x01);
+
+            chip8.pc.should.equal(0x202);
+
+            done();
+        });     
+
+        it (`The interpreter compares register Vx to kk, and if they are equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+
+            chip8.skipNextInstruction_Vx_Equals_kk(0, 0x02);
+
+            chip8.pc.should.equal(0x200);
+
+            done();
+        });                                         
+    }); 
+
+    describe('skipNextInstruction_Vx_NotEquals_kk', function() {
+        it (`The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+
+            chip8.skipNextInstruction_Vx_NotEquals_kk(0, 0x01);
+
+            chip8.pc.should.equal(0x200);
+
+            done();
+        });     
+
+        it (`The interpreter compares register Vx to kk, and if they are not equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+
+            chip8.skipNextInstruction_Vx_NotEquals_kk(0, 0x02);
+
+            chip8.pc.should.equal(0x202);
+
+            done();
+        });                                         
+    });     
+
+    describe('skipNextInstruction_Vx_Equals_Vy', function() {
+        it (`The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+            chip8.Set_LD_Vx_byte(1, 0x01);
+
+            chip8.skipNextInstruction_Vx_Equals_Vy(0, 1);
+
+            chip8.pc.should.equal(0x202);
+
+            done();
+        });     
+
+        it (`The interpreter compares register Vx to register Vy, and if they are equal, increments the program counter by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+            chip8.Set_LD_Vx_byte(1, 0x02);
+
+            chip8.skipNextInstruction_Vx_Equals_Vy(0, 1);
+
+            chip8.pc.should.equal(0x200);
+
+            done();
+        });                                         
+    });     
+
+    describe('skipNextInstruction_Vx_NotEquals_Vy', function() {
+        it (`The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+            chip8.Set_LD_Vx_byte(1, 0x01);
+
+            chip8.skipNextInstruction_Vx_NotEquals_Vy(0, 1);
+
+            chip8.pc.should.equal(0x200);
+
+            done();
+        });     
+
+        it (`The values of Vx and Vy are compared, and if they are not equal, the program counter is increased by 2.`, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+            chip8.Set_LD_Vx_byte(1, 0x02);
+
+            chip8.skipNextInstruction_Vx_NotEquals_Vy(0, 1);
+
+            chip8.pc.should.equal(0x202);
+
+            done();
+        });                                         
+    });             
+
+    describe('loadRegisterI', function() {
+        it (`The value of register I is set to nnn.`, function(done) {
+            var chip8 = new Chip8();
+
+            chip8.loadRegisterI(0xFFF);
+
+            chip8.I.should.equal(0xFFF);
+
+            done();
+        });                                            
+    });    
+
+    describe('jumpAddressV0', function() {
+        it (`The program counter is set to nnn plus the value of V0. `, function(done) {
+            var chip8 = new Chip8();
+            chip8.Set_LD_Vx_byte(0, 0x01);
+
+            chip8.jumpAddressV0(0x300);
+
+            chip8.pc.should.equal(0x301);
+            done();
+        });                                  
+    }); 
+
     describe('executeOpcode', function() {
+        it(`The function executeOpCode(0x00E0) will clear the display`, function() {
+            var chip8 = new Chip8();
+            chip8.display[0] = 1;
+            chip8.executeOpcode(0x00E0);
+            chip8.display[0].should.equal(0);
+        });  
+
         it(`The function executeOpCode(0x6F01) puts the value 0x01 into the register V[0xF]`, function() {
             var chip8 = new Chip8();
             chip8.executeOpcode(0x6F01);

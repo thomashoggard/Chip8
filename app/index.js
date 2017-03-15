803352acc01784abeb43c4a9ff874d6fe5d1252c
Thomas Hoggard <Thomas.Hoggard@gmail.com>
2017-03-15 08:38:39 -0400

    dropdown to select rom

--- app/index.js
+++ app/index.js
@@ -2,16 +2,24 @@ import Chip8 from './Chip8';
 
 let chip8 = new Chip8();
 
-var oReq = new XMLHttpRequest();
-oReq.open("GET", "/app/ROMS/INVADERS.c8", true);
-oReq.responseType = "arraybuffer";
 
-oReq.onload = function (oEvent) {
-  var arrayBuffer = oReq.response; // Note: not oReq.responseText
-  if (arrayBuffer) {
-    var byteArray = new Uint8Array(arrayBuffer);
-    chip8.loadRom(byteArray);
-    chip8.start();
-  }
+document.getElementById('romList').onchange = (e) => {
+  const romName = e.target.value;
+  loadRom(romName);
 };
-oReq.send(null);
+
+function loadRom(romName) {
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", `/app/ROMS/${romName}`, true);
+  oReq.responseType = "arraybuffer";
+
+  oReq.onload = function (oEvent) {
+    var arrayBuffer = oReq.response;
+    if (arrayBuffer) {
+      var byteArray = new Uint8Array(arrayBuffer);
+      chip8.loadRom(byteArray);
+      chip8.start();
+    }
+  };
+  oReq.send(null);
+}
